6c7eb5eab964ec47290d4e4a6f1f33ceeeccf216
yoshiioko <acardoza@pdx.edu>
2024-09-24 11:05:06 -0700

    Create extension method to easily convert Entities to DTOs.

---
+++ VideoGameStore.Api/Entities/EntityExtensions.cs
@@ -0,0 +1,18 @@
+using VideoGameStore.Api.Dtos;
+
+namespace VideoGameStore.Api.Entities;
+
+public static class EntityExtensions
+{
+    public static GameDto AsDto(this Game game)
+    {
+        return new GameDto(
+            game.Id,
+            game.Name,
+            game.Genre,
+            game.Price,
+            game.ReleaseDate,
+            game.ImageUri
+        );
+    }
+}
