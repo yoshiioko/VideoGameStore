c2b27f8844bbfd320142d177ffc14310da407dfa
yoshiioko <acardoza@pdx.edu>
2024-09-24 15:24:46 -0700

    Grab the ConnectionString from Configuration

--- VideoGameStore.Api/Program.cs
+++ VideoGameStore.Api/Program.cs
@@ -4,6 +4,8 @@ using VideoGameStore.Api.Repositories;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSingleton<IGamesRepository, InMemGamesRepository>(); // Only use this while testing with InMemGamesRepository!
 
+builder.Configuration.GetConnectionString("DbContext");
+
 var app = builder.Build();
 
 app.MapGamesEndpoints();
