c0b0ef22dfbc2c9ff495d2050fe80210f153c9dd
yoshiioko <acardoza@pdx.edu>
2024-09-24 11:04:38 -0700

    Create DTOs to establish data contract between client and API.

---
+++ VideoGameStore.Api/Dtos/GameDtos.cs
@@ -0,0 +1,48 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace VideoGameStore.Api.Dtos;
+
+public record GameDto(
+    int Id,
+    string Name,
+    string Genre,
+    decimal Price,
+    DateTime ReleaseDate,
+    string ImageUri
+);
+
+public record CreateGameDto(
+    [Required]
+    [StringLength(50)]
+    string Name,
+
+    [Required]
+    [StringLength(20)]
+    string Genre,
+
+    [Range(1, 100)]
+    decimal Price,
+    DateTime ReleaseDate,
+
+    [Url]
+    [StringLength(100)]
+    string ImageUri
+);
+
+public record UpdateGameDto(
+    [Required]
+    [StringLength(50)]
+    string Name,
+
+    [Required]
+    [StringLength(20)]
+    string Genre,
+
+    [Range(1, 100)]
+    decimal Price,
+    DateTime ReleaseDate,
+
+    [Url]
+    [StringLength(100)]
+    string ImageUri
+);
