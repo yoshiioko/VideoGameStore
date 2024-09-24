c68d3bb6bb15096adfc206427435df40b06bc3b8
yoshiioko <acardoza@pdx.edu>
2024-09-23 19:21:01 -0700

    Implement Get All endpoint

--- VideoGameStore.Api/Program.cs
+++ VideoGameStore.Api/Program.cs
@@ -33,6 +33,6 @@ List<Game> games = [
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/games", () => games);
 
 app.Run();
