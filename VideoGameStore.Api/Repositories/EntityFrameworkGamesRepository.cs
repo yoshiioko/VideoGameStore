4a63b02a3c6def6ad1cf82f0ddaeb1c2a5512ea4
yoshiioko <acardoza@pdx.edu>
2024-09-24 17:24:20 -0700

    Implement EF repository

---
+++ VideoGameStore.Api/Repositories/EntityFrameworkGamesRepository.cs
@@ -0,0 +1,43 @@
+using Microsoft.EntityFrameworkCore;
+using VideoGameStore.Api.Data;
+using VideoGameStore.Api.Entities;
+
+namespace VideoGameStore.Api.Repositories;
+
+public class EntityFrameworkGamesRepository : IGamesRepository
+{
+    private readonly VideoGameStoreContext dbContext;
+
+    public EntityFrameworkGamesRepository(VideoGameStoreContext dbContext)
+    {
+        this.dbContext = dbContext;
+    }
+
+    public IEnumerable<Game> GetAll()
+    {
+        return [.. dbContext.Games.AsNoTracking()];
+    }
+
+    public Game? Get(int id)
+    {
+        return dbContext.Games.Find(id);
+    }
+
+    public void Create(Game game)
+    {
+        dbContext.Games.Add(game);
+        dbContext.SaveChanges();
+    }
+
+    public void Update(Game updatedGame)
+    {
+        dbContext.Games.Update(updatedGame);
+        dbContext.SaveChanges();
+    }
+
+    public void Delete(int id)
+    {
+        _ = dbContext.Games.Where(game => game.Id == id)
+                           .ExecuteDelete();
+    }
+}
