877ce9f12d9a0ce5081015adc1cd34a49a0f32b3
yoshiioko <acardoza@pdx.edu>
2024-09-24 17:23:26 -0700

    Reorder methods

--- VideoGameStore.Api/Repositories/IGamesRepository.cs
+++ VideoGameStore.Api/Repositories/IGamesRepository.cs
@@ -4,9 +4,9 @@ namespace VideoGameStore.Api.Repositories;
 
 public interface IGamesRepository
 {
-    void Create(Game game);
-    void Delete(int id);
-    Game? Get(int id);
     IEnumerable<Game> GetAll();
+    Game? Get(int id);
+    void Create(Game game);
     void Update(Game updatedGame);
+    void Delete(int id);
 }
