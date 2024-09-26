ae0818f7b76b71e0d1e3806a4fa3f7acb558bf41
yoshiioko <acardoza@pdx.edu>
2024-09-26 09:58:35 -0700

    Configure CORS and add it to the pipeline.

--- VideoGameStore.Api/Program.cs
+++ VideoGameStore.Api/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Cors.Infrastructure;
 using VideoGameStore.Api.Authorization;
 using VideoGameStore.Api.Data;
 using VideoGameStore.Api.Endpoints;
@@ -11,11 +12,21 @@ builder.Services.AddApiVersioning(options =>
     options.DefaultApiVersion = new(1.0);
     options.AssumeDefaultVersionWhenUnspecified = true;
 });
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(corsBuilder =>
+    {
+        var allowedOrigin = builder.Configuration["AllowedOrigin"] ?? throw new InvalidOperationException("AllowedOrigin is not set!");
+        corsBuilder.WithOrigins(allowedOrigin)
+                   .AllowAnyHeader()
+                   .AllowAnyMethod();
+    });
+});
 
 var app = builder.Build();
 
 await app.Services.InitializeDb();
-
 app.MapGamesEndpoints();
+app.UseCors();
 
 app.Run();
